1162c1e1a6bdd08fb6e2f47cdc18d8125ad013cc
Dario Bernabeu <dariosistemas@gmail.com>
2024-09-20 14:32:33 -0300

    Add files via upload

---
+++ minio_upload_file.py
@@ -0,0 +1,23 @@
+import io
+from minio import Minio
+
+def main():
+    client = Minio(
+        endpoint="localhost:9000",
+        access_key="<ACCESS_KEY>",
+        secret_key="<SECRET_KEY>",
+        secure=False
+    )
+
+    data = "Hello World"
+    data = io.BytesIO(data.encode('utf-8'))
+
+    client.put_object(
+        "bronze",
+        "messages/test/hello_world.md",
+        data,
+        length=len(data.getbuffer())
+    )
+
+if __name__ == '__main__':
+    main()
