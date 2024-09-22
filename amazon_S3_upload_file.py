72267254dce26ff8768d48614c416c460838a5ec
Dario Bernabeu <dariosistemas@gmail.com>
2024-09-22 07:56:20 -0300

    Update amazon_S3_upload_file.py

--- amazon_S3_upload_file.py
+++ amazon_S3_upload_file.py
@@ -4,18 +4,18 @@ import boto3
 def main():
     client = boto3.client(
         's3',
-        endpoint_url="http://localhost:9000",
-        aws_access_key_id="<ACCESS_KEY>",
-        aws_secret_access_key="<SECRET_KEY>"
+        endpoint_url='http://localhost:9000',
+        aws_access_key_id='<ACCESS_KEY>',
+        aws_secret_access_key='<SECRET_KEY>'
     )  
 
-    data = "Hello World"
+    data = 'Hello World'
     data = io.BytesIO(data.encode('utf-8'))
 
     client.upload_fileobj(
         data, 
-        "bronze",
-        "messages/test/hello_world.md"
+        'bronze',
+        'messages/test/hello_world.md'
     )
 
 if __name__ == '__main__':
